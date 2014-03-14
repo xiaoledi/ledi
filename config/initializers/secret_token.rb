5c51ad7be65457726997cc05bd095f71536db03a
yingyiledi <yingyiledi@gmail.com>
2014-03-14 11:13:24 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ledi::Application.config.secret_key_base = '37365228a510f22d55c4773ce3081d59314e0f3bda09f6f4504682ae94e821efe3f7dd487224b3bc2a89c0a96a6de1bf792fd7ce9662c46e3d657919e88224f1'
